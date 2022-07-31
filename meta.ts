448e105766bd2703bceb0bb56fb6679dbbedae61
Cansiny0320 <1136843910@qq.com>
2022-07-31 17:15:02 +0800

    chore: update genmeta

---
+++ meta.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { readPackageJSON } from 'pkg-types'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const pkg = await readPackageJSON(path.resolve(dirname, './package.json'))
+
+export const meta = `// ==UserScript==
+// @name         b 站黑名单加强
+// @description  b 站黑名单加强
+// @version      ${pkg.version}
+// @namespace    ${pkg.homepage}
+// @author       ${pkg.author}
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @match        https://www.bilibili.com/*
+// @supportURL   ${pkg.bugs}
+// @updateURL    https://github.com/Cansiny0320/bilibili-block-plus/raw/master/dist/index.meta.js
+// @downloadURL  https://github.com/Cansiny0320/bilibili-block-plus/raw/master/dist/index.user.js
+// @run-at       document-start
+// @license      ${pkg.license} License
+// @grant        GM.getValue
+// @grant        GM.setValue
+// ==/UserScript==
+`
