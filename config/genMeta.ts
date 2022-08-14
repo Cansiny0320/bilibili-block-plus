e596d68ce03c2a349dac2346732891c9095f29b9
Cansiny0320 <1136843910@qq.com>
2022-08-14 15:44:04 +0800

    chore: update devDependencies

--- config/genMeta.ts
+++ config/genMeta.ts
@@ -6,4 +6,4 @@ import { meta } from './meta'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-fs.writeFileSync(path.resolve(dirname, './dist/index.meta.js'), meta)
+fs.writeFileSync(path.resolve(dirname, '../dist/index.meta.js'), meta)
