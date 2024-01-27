e6b62c148f0e140be46478c2d51d2e64611f15d7
Cansiny0320 <1136843910@qq.com>
2024-01-28 06:45:38 +0800

    build: 不压缩

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'tsup'
 import { meta } from './config/meta'
-export default defineConfig(options => {
+export default defineConfig(() => {
   return {
     entry: ['src/index.ts'],
     clean: true,
     target: 'es6',
     format: 'iife',
-    minify: !options.watch,
+    minify: false,
     replaceNodeEnv: true,
     banner: {
       js: `\n${meta}`,
